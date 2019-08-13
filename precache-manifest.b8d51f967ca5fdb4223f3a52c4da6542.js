678240e92f33ef0670795bd4eff96b11dc2e40ba
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-08-14 06:50:47 +0800

    Updates

---
+++ precache-manifest.b8d51f967ca5fdb4223f3a52c4da6542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a5b88c3e017452f51bcc9d3ce4079f",
+    "url": "/full-beaker/index.html"
+  },
+  {
+    "revision": "8db3493e49aa851d113a",
+    "url": "/full-beaker/static/css/main.c3776d5e.chunk.css"
+  },
+  {
+    "revision": "9e8eb2aedcf06e1cd1a2",
+    "url": "/full-beaker/static/js/2.ea87d87b.chunk.js"
+  },
+  {
+    "revision": "8db3493e49aa851d113a",
+    "url": "/full-beaker/static/js/main.9a0a5ab9.chunk.js"
+  },
+  {
+    "revision": "fe134b5b5a47e0d75938",
+    "url": "/full-beaker/static/js/runtime~main.ab325ff4.js"
+  }
+]);
